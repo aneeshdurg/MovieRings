25c157ea827b76b09de73e77f27b073ac4d0cbf4
A-Goel <agoel9@illinois.edu>
2015-11-07 17:41:46 -0500

    Major improvements

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from PIL import Image
 import os
 import pygame
+import subprocess
+import re
+from decimal import Decimal
 
 def drawRings(frameAvg, movie, flag):
 	if flag:
@@ -10,32 +13,46 @@ def drawRings(frameAvg, movie, flag):
 		foo = open("index.txt", "r")
 		temp = foo.readlines()
 		foo.close()
-		for i in xrange(len(temp) / 4):
-			frameAvg.append([temp[3 * i], temp[(3 * i) + 1], temp[(3 * i) + 2], temp[(3 * i) + 3]]) 	
+		for i in xrange(len(temp) / 3):
+			frameAvg.append([temp[3 * i], temp[(3 * i) + 1], temp[(3 * i) + 2]]) 	
 	else:
 		##Writing to file
 		foo = open("index.txt", "w")
 		for i in xrange(len(frameAvg)):
-			for j in xrange(4):		
+			for j in xrange(3):		
 				foo.write(str(frameAvg[i][j]))
 				foo.write("\n")
 		foo.close()	
 			
 	#Initializing pygame
 	pygame.init()
-	SIZE = [1050, 750]
+	SIZE = [1380, 850]
 	screen = pygame.display.set_mode(SIZE)
 	pygame.display.set_caption(movie)
 
 	#Drawing circles using the average color of each frame
 	for i in xrange(len(frameAvg)):
-		(r, b, g, a)=(int(frameAvg[i][0]), int(frameAvg[i][1]), int(frameAvg[i][2]), int(frameAvg[i][3]))
-		color=(r, b, g, a)
-		pygame.draw.circle(screen, color, [SIZE[0] / 2, SIZE[1] / 2], 6 * (len(frameAvg)) + 6 - 6 * (i + 1), 5)
+		r, b, g=(int(frameAvg[i][0]), int(frameAvg[i][1]), int(frameAvg[i][2]))
+		color=(r, b, g)
+		pygame.draw.circle(screen, color, [SIZE[0] / 2, SIZE[1] / 2], 1 * (len(frameAvg)) + 1 - 1 * (i + 1))
 	pygame.display.flip()
 
 	pygame.image.save(screen, "RESULT.JPG")
 
+
+def get_video_length(path):
+	process = subprocess.Popen(['ffmpeg', '-i', path], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	stdout, stderr = process.communicate()
+	matches = re.search(r"Duration:\s{1}(?P<hours>\d+?):(?P<minutes>\d+?):(?P<seconds>\d+\.\d+?),", stdout, re.DOTALL).groupdict()
+	hours = Decimal(matches['hours'])
+	minutes = Decimal(matches['minutes'])
+	seconds = Decimal(matches['seconds'])
+ 
+	total = 0
+	total += 60 * 60 * hours
+	total += 60 * minutes
+	total += seconds
+	return total
 
 movielist=[name for name in os.listdir('.') if os.path.isdir(name)]
 #Getting movie name:
@@ -51,8 +68,9 @@ while True:
 	if os.path.exists(movie) and os.path.isdir(movie):
 		#Changing Direcotry and getting file names:
 		os.chdir(os.getcwd() + '/' + movie + '/')
-		if os.path.exists("index.txt"):
+		if os.path.exists("index.txt") and not os.stat("index.txt").st_size == 0:
 			flag = True
+    		
 		if flag:
 			drawRings([], movie, flag)
 			os.chdir('..')	
@@ -61,44 +79,48 @@ while True:
 
 		moviefile = ""
 		for name in os.listdir('.'):
+			#print name
 			if name.startswith(movie) and not name.endswith('.jpg'):
 				moviefile = name
-
+		duration = get_video_length(moviefile)
+		frames = 410 / duration
 		files=[name for name in os.listdir('.') if os.path.isfile(name) and name.endswith('.jpg')]
-		if len(files) <= 3:
-			os.system("ffmpeg -i \"" + moviefile + "\" -r 0.0167 %02d.jpg")
+		if len(files) <= 1:
+			os.system("ffmpeg -i " + moviefile + " -r " + str(frames) + " -s 100x100 -f image2 %03d.jpg")
 		for name in files:
-			os.system('cls' if os.name == 'nt' else 'clear')
+			#os.system('cls' if os.name == 'nt' else 'clear')
 			print ("proccessing frame " +name[:-4] + " of " + files[len(files)-1][:-4])
 			im = Image.open(name)
-			pix = im.convert('RGBA')
+			pix = im.load()
 			x, y = im.size 
 			RED_VALS = []
 			BLUE_VALS = []
 			GREEN_VALS =[]
-			ALPHA_VALS = []
+			#ALPHA_VALS = []
 			avgRed = 0
 			avgBlue = 0
 			avgGreen = 0
-			avgAlpha = 0
+			#avgAlpha = 0
 			for i in xrange(100):
 				for j in xrange(100):
-					tempR, tempB, tempG, tempA = pix.getpixels((i, j))
+					#print pix.getpixels((i, j))
+					#raw_input()
+					tempR, tempB, tempG = pix[i, j]
 					RED_VALS.append(tempR)
 					BLUE_VALS.append(tempB)
 					GREEN_VALS.append(tempG)
-					ALPHA_VALS.append(tempA)
+					#ALPHA_VALS.append(tempA)
 			for i in xrange(10000):
 				avgRed += RED_VALS[i]
 				avgBlue += BLUE_VALS[i]
 				avgGreen += GREEN_VALS[i]
-				avgAlpha += ALPHA_VALS[i]
+				#avgAlpha += ALPHA_VALS[i]
 
 			avgRed /= 10000	
 			avgBlue /= 10000
 			avgGreen /= 10000
-			avgAlpha /= 10000
-			frameAvg.append([avgRed, avgBlue, avgGreen, avgAlpha])
+			#avgAlpha /= 10000
+			frameAvg.append([avgRed, avgBlue, avgGreen])
 		drawRings(frameAvg, movie, flag)	
 		os.chdir('..')	
 		continue
